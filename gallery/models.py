f07b9f31519e912d81cb4246ad78d6ffb2f55a3f
Bryan Juma <jumabryan10@gmail.com>
2018-09-28 17:07:56 +0300

    defaul ddesccription added

--- gallery/models.py
+++ gallery/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 from __future__ import unicode_literals
+
+Default_desc = '''nothing that is good in life will ever come easy life is not an easy road,
+fally lover,
+n if u dunno now u know.
+hustle .'''
