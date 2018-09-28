908ce1e445a73bdbe7e6b336f003a22b6a559ada
Bryan Juma <jumabryan10@gmail.com>
2018-09-28 18:46:04 +0300

    edit urls

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from __future__ import unicode_literals
+from django.db import models
+
 
 Default_desc = '''nothing that is good in life will ever come easy life is not an easy road,
 fally lover,
