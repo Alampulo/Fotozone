55f889dc9fb65586f4154e939ffebbd915838a14
Bryan Juma <jumabryan10@gmail.com>
2018-09-28 16:34:14 +0300

    db installed

--- Fotozone/settings.py
+++ Fotozone/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Fotozone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Fotozone',
+        'USER':'moringa',
+        'PASSWORD': 'Moringa123'
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
