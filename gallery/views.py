3c837e27a5c980752406c1e8a6c64bbe0a37d6ad
Bryan Juma <jumabryan10@gmail.com>
2018-09-28 17:14:29 +0300

    sets view module

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .models import *
-
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
+def home(request):
+    images = Image.objects.all()
+    categories = Category.objects.all()
+    return render(request,'home.html',locals())
+
+def search(request):
+    searchstring=request.POST['search']
+    images=Image.search_image(searchstring)
+    name=searchstring
+    title=name
+    return render(request,'results.html',locals())
+
+def getcat(request,catid):
+    category=get_object_or_404(Category,pk = catid)
+    title=category.Name
+    name=category.Name
+    images=category.images.all()
+    return render(request,'results.html',locals())
