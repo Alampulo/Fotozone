61dc582039d5bb41d9317ef07dbb644004fd5f91
Bryan Juma <jumabryan10@gmail.com>
2018-10-01 17:07:45 +0300

    navbar input

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import Location,Image,Category
-
-class ImageAdmin(admin.ModelAdmin):
-    filter_horizontal = ('category',)
 
 # Register your models here.
-admin.site.register(Location)
-admin.site.register(Image, ImageAdmin)
-admin.site.register(Category)
--- gallery/models.py
+++ gallery/models.py
@@ -1,29 +1,3 @@
 from django.db import models
+
 # Create your models here.
-
-class Category(models.Model):
-    name = models.CharField(max_length = 30)
-
-    def __str__(self):
-        return self.name
-
-class Location(models.Model):
-    name = models.CharField(max_length = 30)  
-
-    @classmethod
-    def filter_by_location(cls,name):
-        location= cls.objects.filter(name = name) 
-        return location      
-    
-
-    def __str__(self):
-        return self.name
-
-class Image(models.Model):
-    title = models.CharField(max_length = 60)
-    name = models.CharField(max_length = 60)
-    description = models.CharField(max_length = 90)
-    location = models.ForeignKey(Location)
-    category = models.ManyToManyField(Category)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to = 'images/')
