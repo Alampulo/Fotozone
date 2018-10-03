d2cde41662e13dac903466d4d746455be2cccf28
Bryan Juma <jumabryan10@gmail.com>
2018-10-03 16:39:16 +0300

    addds serch

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url,include
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url(r'^$',views.home)
+    url(r'^$',views.home, name = 'home'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(\d+)', views.get_location, name = 'location')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,45 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
-# Create your views here.
-def welcome(request):
-    return render(request, 'base.html')
+from .models import Image,Category,Location
 
 def home(request):
 
-    message = 'hiiiii'
-    return render(request, 'welcome.html')
+    image = Image.get_images()
+    #     category_results = Category.objects.all()
+    #     location_results = Location.objects.all()
+
+    return render(request,'All-images/welcome.html',{"image" : image})
+
+
+# def home(request):
+#     image = Image.get_images()
+#   all_images = Image.objects.all()
+#     category_results = Category.objects.all()
+#    location_results = Location.objects.all()
+#     return render(request,'All-images/welcome.html',{"image" : image,'location_results':location_results,'category_results':category_results})
+
+
+def search_results(request):
+
+    if 'image_category' in request.GET and request.GET["image_category"]:
+        search_term = request.GET.get("image_category")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'All-images/search.html',{"message":message,"all_images": searched_image})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'All-images/search.html',{"message":message})
+
+def get_category(request,category):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    category_result = Image.objects.filter(image_category__cat_name = category)
+    return render(request,'All-images/search.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
+
+def get_location(request,location):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    location_result = Image.objects.filter(image_location__location_name= location)
+    return render(request,'locations.html',{'all_images':location_result,'category_results':category_results,'location_results':location_results})
