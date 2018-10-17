894e52d24a8fdfacb20654dd4a9c6becab0827d2
Bryan Juma <40592552+Alampulo@users.noreply.github.com>
2018-10-17 11:29:20 +0300

    Update tests.py
    tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Category, Location, Image
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.category= Category(category = 'Shepherd')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.category.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.location= Location(location = 'France')
+        self.location.save_location()
+
+        self.new_category = Category(category = 'Shepherd')
+        self.new_category.save(
