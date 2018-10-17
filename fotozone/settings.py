22cfbebb97241949cb322b7d324984cea561c166
Bryan Juma <jumabryan10@gmail.com>
2018-10-17 09:49:24 +0300

    deploy

--- fotozone/settings.py
+++ fotozone/settings.py
@@ -66,7 +66,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 INSTALLED_APPS = [
     'bootstrap3',
-    'bootstrap4',
     'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -85,6 +84,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fotozone.urls'
